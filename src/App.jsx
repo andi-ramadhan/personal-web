743936e9ec4a30e58f88476156300c0a35bb5024
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2026-01-11 14:29:12 +0700

    add works section component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import ProfileHead from './components/ProfileHead'
+import Works from './components/Works'
 
 function App() {
 
   return (
     <>
-      <div className='w-full h-screen flex justify-center items-center absolute bg-radial from-purple-300 via-10% to-yellow-100'></div>
+      <div className='w-full h-screen fixed justify-center items-center bg-radial from-purple-300 via-10% to-yellow-100'></div>
       <header className='flex justify-center'>
         <Navbar />
       </header>
-      <main className='flex flex-col justify-center h-screen mx-4'>
+      <main className='flex flex-col justify-center items-center px-3 min-h-screen w-full overflow-hidden'>
         <ProfileHead />
+        <Works />
       </main>
     </>
   )
