0464422a1cba7d4cb46ccff4295b44ba2f936fe0
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2026-01-11 14:57:01 +0700

    feat: In progress for adding modal pop up feature for mobile view project works section.

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,20 +1,41 @@
+import { useState } from "react"
+
+const Modal = () => {
+  return (
+    <div className="fixed size-full bg-white/20 backdrop-blur-lg p-5">
+      <p>Modal</p>
+    </div>
+  )
+}
+
 export default function Works() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => { setIsModalOpen(true) };
+  const closeModal = () => { setIsModalOpen(false) };
+
+  const handleModal = () => {
+    if (isModalOpen) {
+      closeModal();
+    } else {
+      openModal();
+    }
+  }
+
   return (
     <div className="flex flex-col justify-center items-center leading-relaxed text-pretty h-screen w-full">
       <div className="flex flex-col gap-3 shadow-2xl backdrop-blur-xl bg-white/20 rounded-xl p-5 [&>p]:text-md w-full">
         <h1 className="text-4xl pb-3" id="works">My Works</h1>
         <div className="container flex flex-col gap-2 w-full">
 
-          <div className="grid grid-cols-8 rounded-xl shadow-xl px-4 py-2">
-            <h3 className="text-lg col-span-7">SiiChiSei Vocal Course Website</h3>
-            <a href={"https://siichisei-vocalcourse.pages.dev"} className="text-sm col-span-1 shadow-lg flex justify-center items-center rounded-xl bg-purple-400">
-              <img
-                src="src/assets/arrow.svg"
-                className="size-5 rotate-90 animate-bounce -translate-x-1"
-              />
+          <div className="flex items-center rounded-xl shadow-sm px-4 py-2 ring-1 ring-purple-300">
+            <a>
+              <h3 className="text-lg col-span-7">SiiChiSei Vocal Course Website</h3>
             </a>
           </div>
-
+          <div className="flex items-center rounded-xl shadow-sm px-4 py-2 ring-1 ring-purple-300">
+            <h3 className="text-lg col-span-7">SiiChiSei Vocal Course Website</h3>
+          </div>
         </div>
       </div>
     </div>
