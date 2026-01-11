12a5549ad383dd97029a1ec6010c5252e1648801
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2026-01-11 14:57:48 +0700

    changes: Move navbar to top rather than bottom, use tailwind children selector for better clean code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,24 @@
 export default function Navbar() {
   return (
-    <div className="flex gap-10 fixed bottom-5 z-1 bg-none backdrop-blur-xl rounded-xl shadow-2xl p-4 justify-center">
+    <div className={`
+      flex gap-10 fixed top-5 z-1 bg-none backdrop-blur-sm rounded-xl ring-1 ring-purple-200 shadow-md p-4 justify-center
+      [&>a]:size-7 [&>a]:opacity-70 [&>a]:font-thin
+      `} >
       <a href="#">
         <img
           src="/images/home.svg"
-          className="size-7 opacity-50"
         />
       </a>
       <a href="#works">
         <img
           src="/images/work.svg"
-          className="size-7 opacity-50"
         />
       </a>
       <a href="#contact">
         <img
           src="/images/contact.svg"
-          className="size-7 opacity-50"
         />
       </a>
-    </div>
+    </div >
   )
 }
