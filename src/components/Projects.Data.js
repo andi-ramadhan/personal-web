de00ae91230401928f4d5525157f6379463e2a7d
Andi Ramadhan <ramadhanfajar.ara@gmail.com>
2026-01-16 16:56:41 +0700

    feat: project data for personal projects display.

---
+++ src/components/Projects.Data.js
@@ -0,0 +1,16 @@
+export const projectData = [
+  {
+    title: "SiiChiSei Vocal Course Website",
+    description: "A Company Web profile for SiiChiSei Vocal Course, a Vocal Course Company that held in Online based for any people in Indonesia.",
+    details: "In this project, I ensure the clarity, accessibility, and user-friendly interface for the website.",
+    technologies: ["React.js", "Vite", "TailwindCSS", "Motion", "EmailJS"],
+    link: "https://siichisei-vocalcourse.pages.dev/"
+  },
+  {
+    title: "OpenMusic-API",
+    description: "A Restful API Music Library, provides security in creating a music library. User also can collaborate to make their music library together",
+    details: "This project is designed for managing music-related data, such as songs, albums, users, playlist, and user collaboration features.",
+    technologies: ["Hapi.js", "PostgreSQL", "Redis", "JWT", "RabbitMQ", "Joi", "Bcrypt", "Node.js"],
+    link: "https://github.com/andi-ramadhan/openmusic-api"
+  }
+];
